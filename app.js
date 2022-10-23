bc38c2189a03410df1b0c11c72d7cbdd648be0e1
Ramunas <ramunas.skardzius@gmail.com>
2022-10-23 21:16:40 +0300

    added readline package

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Hello!")
+var readline = require('readline');
+var rl = readline.createInterface(
+    process.stdin, process.stdout);
+
+rl.question('Iveskite varda: ', (name) => {
+    console.log('Sveiki, ' + name +"!");
+});
